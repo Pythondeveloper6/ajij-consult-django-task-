9b39f6a554f4a288e8e9f345fe57408e71f951f8
pythondeveloper6 <pythondeveloper6@gmail.com>
2021-01-19 20:58:11 +0200

    add the type a head widget using the autocompelete jqueryui

--- stamp/views.py
+++ stamp/views.py
@@ -4,12 +4,19 @@ from django.http import JsonResponse
 from django.template.loader import render_to_string
 
 def stamp_list(request):
+
+    if 'term' in request.GET:
+        qs = Theme.objects.filter(name__icontains=request.GET.get('term'))
+        print(request.GET.get('term'))
+        themes = [theme.name for theme in qs]
+        return JsonResponse(themes, safe=False)
+
     themes = Theme.objects.all()
     stamps = Stamp.objects.all()
     
     if request.method == "GET" and request.is_ajax():
         theme = request.GET.get('theme')
-        stamps = Stamp.objects.filter(theme=theme)
+        stamps = Stamp.objects.filter(theme__name=theme)
         
         context = {'themes':themes , 'stamps':stamps}
         html = render_to_string('stamp/stamp_table_list.html' , context , request=request)
