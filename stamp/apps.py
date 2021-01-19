77105dd3d5c5c4097691ae74f15366d2c56850a2
pythondeveloper6 <pythondeveloper6@gmail.com>
2021-01-19 18:02:11 +0200

    design the models

---
+++ stamp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StampConfig(AppConfig):
+    name = 'stamp'
---
+++ stamp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Stamp(models.Model):
+    year = models.CharField(max_length=12)
+    denomination = models.CharField(max_length=100)
+    theme = models.ForeignKey('Theme', related_name='', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.year
+
+
+class Theme(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
